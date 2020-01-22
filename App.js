6c2a3e4e9a49b7737fa1a3a914fb4c03a71d7c29
Augusto <l16371125@apizaco.tecnm.mx>
2020-01-21 19:07:18 -0600

    add: 1 view, 3 styles, 3 texts

--- App.js
+++ App.js
@@ -4,7 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.textBold}>Hello world</Text>
+      </View>
+      <View style={styles.body}>
+          <Text style={styles.textBold}>Lorem ipsu dolro sit amet. <Text>Jajaja</Text></Text>
+          <Text>Lorem ipsu dolro sit amet.</Text>
+          <Text>Lorem ipsu dolro sit amet.</Text>
+      </View>
     </View>
   );
 }
@@ -15,5 +22,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor : 'purple',
+    padding : 20,
+  },
+  textBold: {
+    fontWeight : 'bold',
+    color : 'white',
+  },
+  body: {
+    backgroundColor : 'green',
+    padding : 50,
   },
 });
