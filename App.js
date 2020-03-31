a31f78ffb3e80d454a0f7361e3b1abf009a1fb12
snow915 <cris_ganor@hotmail.com>
2020-03-30 21:07:14 -0600

    add: pressHandler function in App.js, which i pass the handler to todoItem.js

--- App.js
+++ App.js
@@ -4,11 +4,19 @@ import Header from "./components/header";
 import TodoItem from "./components/todoItem";
 
 export default function App() {
+
     const [todos,setTodos] = useState([
         {text: 'buy coffee', key: '1'},
         {text: 'create an app', key: '2'},
         {text: 'play on the switch', key: '3'}
     ]);
+
+    const pressHandler = (key) => {
+        setTodos((prevTodos) => {
+            return prevTodos.filter(todo => todo.key != key);
+        });
+    }
+
     return (
         <View style={styles.container}>
             <Header />
@@ -18,7 +26,10 @@ export default function App() {
                     <FlatList
                         data={todos}
                         renderItem={({item}) => (
-                            <TodoItem item={item}/>
+                            <TodoItem
+                                item={item}
+                                pressHandler={pressHandler}
+                            />
                         )}
                     />
                 </View>
