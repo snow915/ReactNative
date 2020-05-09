5e3acebf7dd9d4834103bb61f15b50061c1605a4
snow915 <cris_ganor@hotmail.com>
2020-05-09 11:31:45 -0500

    add: alert when the input have less than 3 chars

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity, Alert} from 'react-native';
 import Header from "./components/header";
 import TodoItem from "./components/todoItem";
 import AddTodo from "./components/addTodo";
@@ -13,13 +13,21 @@ export default function App() {
     ]);
 
     /*ESTA ES LA FUNCION QUE VA A AÑADIR EL NUEVO OBJETO AL STATE, QUE ES EL QUE SE ITERA PARA MOSTRAR LOS ELEMENTOS*/
-    const submitHandler = (text) => { //RECIBE UN PARAMETRO, QUE ES EL TEXTO INGRESADO EN EL INPUT DESDE addTodo.js
-        setTodos((prevTodos) => {
-            return [
-                {text : text, key : Math.random().toString()}, /*AQUI AÑADIMOS EL NUEVO OBJETO*/
-                ...prevTodos //Operador de propagación
-            ]
-        })
+    const submitHandler = (text) => {
+        //RECIBE UN PARAMETRO, QUE ES EL TEXTO INGRESADO EN EL INPUT DESDE addTodo.js
+
+        if(text.length > 3){
+            setTodos((prevTodos) => {
+                return [
+                    {text : text, key : Math.random().toString()}, /*AQUI AÑADIMOS EL NUEVO OBJETO*/
+                    ...prevTodos //Operador de propagación
+                ]
+            })
+        } else {
+            Alert.alert("Ops", "Todos must be over 3 chars long", [
+                {text: "Understood", onPress: () => console.log("alert closed")}
+            ]);
+        }
     }
 
     /*ESTA FUNCION ELIMINA EL ELEMENTO DEL STATE QUE LE DEMOS CLICK*/
